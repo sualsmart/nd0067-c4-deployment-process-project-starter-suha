91ee5b23e570926d7003752ddd32ae84f89ff5fa
SudKul <sudhanshu.kulshrestha@gmail.com>
2022-05-31 15:33:38 +0530

    Add EB

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "udagram-api-dev-751397240857.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
